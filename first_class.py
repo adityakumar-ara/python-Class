ab2bb98aebca5ec61a8fd5bfb6d29fdd57bbc896
adityakumar-ara <adityaara7667@gmail.com>
2025-10-29 00:10:35 +0530

    python program

---
+++ first_class.py
@@ -0,0 +1,72 @@
+# print("Hell world")
+# arithmetic opreater
+# a=4
+# b=8
+# print("sum is",a+b )
+# print("subtraction",a-b)
+# print("multiplai",a*b)
+# print("Division",a/b)
+# print("flooe division", a//b)
+# print("modulus",a%b)
+#assignment opwrater
+#a=5
+# b=10
+# print("sum is",a+b)
+# a+=b
+# print("sum is",a)
+# a-=b
+# print("subtraction is",a)
+# a-=b
+# print("division",a)
+# a//=b
+# print("floor devision",a)
+# a%=b
+# print("modulus is",a)
+# a*=b
+# print("maltiplication",a)
+#coparison operator
+# a=5
+# b=10
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a>b)
+# print(a>=b)
+# print(a<=b)
+###logical operator
+# a=5
+# b=10
+# #and operator
+# print(a==b and a<b)
+# print(b>=a and a<=b)
+# #or operator
+# print(a<b or b>a)
+# print(a>=b or b<=a)
+##input function
+# first_name=input("enter your first name")
+# last_name=input("inter your last name")
+# your_full_name=first_name +" "+last_name
+# print("your full name is",your_full_name)
+#Bitwise operator
+# a=8
+# b=9
+# Bitwise and operator
+# print(a&b)
+
+# Bitwise or operator
+# print(a|b)
+
+# Bitwise xor operator
+# print(a^b)
+
+#Membership operatop
+# Number=[1,2,3,4,5,6]
+# print(5 in Number)
+# print(7 in Number)
+#string concetenation("+")
+#first_name="aditya"
+# last_name="kumar"
+# full_name=first_name+' '+last_name
+# print("full name is",full_name)
+#string replication
+#print(first_name*9)
