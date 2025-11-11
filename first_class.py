113604eb8d4a9a8cb492c345a1b6d99d2749b9db
Aditya Kumar <adityaara7667@gmail.com>
2025-11-11 21:03:21 +0530

    hello
    hello

--- first_class.py
+++ first_class.py
@@ -1,5 +1,5 @@
 # print("Hell world")
-# arithmetic opreater
+# #arithmetic opreater
 # a=4
 # b=8
 # print("sum is",a+b )
@@ -8,8 +8,8 @@
 # print("Division",a/b)
 # print("flooe division", a//b)
 # print("modulus",a%b)
-#assignment opwrater
-#a=5
+# #assignment opwrater
+# a=5
 # b=10
 # print("sum is",a+b)
 # a+=b
@@ -24,7 +24,7 @@
 # print("modulus is",a)
 # a*=b
 # print("maltiplication",a)
-#coparison operator
+# #coparison operator
 # a=5
 # b=10
 # print(a==b)
@@ -33,7 +33,7 @@
 # print(a>b)
 # print(a>=b)
 # print(a<=b)
-###logical operator
+# ##logical operator
 # a=5
 # b=10
 # #and operator
@@ -42,46 +42,46 @@
 # #or operator
 # print(a<b or b>a)
 # print(a>=b or b<=a)
-##input function
+# #input function
 # first_name=input("enter your first name")
 # last_name=input("inter your last name")
 # your_full_name=first_name +" "+last_name
 # print("your full name is",your_full_name)
-#Bitwise operator
+# #Bitwise operator
 # a=8
 # b=9
-# Bitwise and operator
+# #Bitwise and operator
 # print(a&b)
 
-# Bitwise or operator
+# #Bitwise or operator
 # print(a|b)
 
-# Bitwise xor operator
+# #Bitwise xor operator
 # print(a^b)
 
-#Membership operatop
+# #Membership operatop
 # Number=[1,2,3,4,5,6]
 # print(5 in Number)
 # print(7 in Number)
-#string concetenation("+")
-#first_name="aditya"
+# #string concetenation("+")
+# first_name="aditya"
 # last_name="kumar"
 # full_name=first_name+' '+last_name
 # print("full name is",full_name)
-#string replication
-#print(first_name*9)
+# #string replication
+# print(first_name*9)
 
 
-###2nd parameter
-###indexing 
-#possitive indexing
+# ##2nd parameter
+# ##indexing 
+# #possitive indexing
 # name="aditya" #a=1,d=2,i=3,t=4,y=5,a=6
 # print(name[:])
 # print(name[0:3])
 # print(name[3:6])
 # print(name[0:])
 
-### negative indexing
+# ## negative indexing
 # name="aditya" #a=-6,d=-5,i=-4,t=-3,y=-2,a=-1
 # print(name[:])
 # print(name[-3:-1])
@@ -89,13 +89,13 @@
 # print(name[-6:])
 
 
-##3rd parameter
+# #3rd parameter
 # name="adityakumar"
 # print(name[0: :2])
 # print(name[2: :3])
-# print(name[:-1:2])
+# print(name[:-1:2])#
 
-###coditional statement
+# ##coditional statement
 # a=2
 # n=3
 # if a<n:
@@ -103,12 +103,38 @@
 # else:
 #     print("a is less than to b")  
 
-age=int(input("hello,there enter your age"))
-Resident=input("enter you resident").title()
-if age>=18 and Resident=="Bihar":
-    print("you they can vote, there in bihar")
-elif age<18 and Resident=="Bihar":
-   print("you are not eligible for vote,because only they can vote 18+ ")
-else:
-  print("you are not eligible for vote ")   
-  print("hello")    
+# age=int(input("hello,there enter your age"))
+# Resident=input("enter you resident").title()
+# if age>=18 and Resident=="Bihar":
+#     print("you they can vote, there in bihar")
+# elif age<18 and Resident=="Bihar":
+#    print("you are not eligible for vote,because only they can vote 18+ ")
+# else:
+#   print("you are not eligible for vote, there in bihar ")   
+
+# User_name=input("enter your User_name")
+# Password=input("enter your password")
+# if User_name=="aditya" and Password=="06/06/2006":
+#         print("login sucsses full")
+# elif User_name=="aditya" and Password=="06/06/06":
+#       print("sorry, your password is wrong")
+# else:
+#        print("please enter your curect User_name and password")
+
+
+# a=int(input("Enter you number"))
+# if(a%2==0):
+#   print("this is not odd number")
+# else:
+#   print("this is odd number")  
+ 
+# proccess="admition"
+# marks_of_10th= int(input("Enter your marks of 10th"))
+# if marks_of_10th>=300:
+#    marks_of_12th=int(input("Enter your 12th marks"))
+#    if marks_of_12th>=300 and marks_of_12th>500:
+#     id=input("Do you have PAN card yes/no").lower()
+#     if id=="yes":
+#      print("congrats! you are eligible for admissoin")
+# else:
+#       print("you are not eligible for admissoin")
---
+++ if_else.py
@@ -0,0 +1,34 @@
+# instagram="login"
+# user_name="rajkumar@1223"
+# password="rajkumar@bihar"
+# input_username=input("enter your user_name:")
+# input_password=input("enter your password:")
+# if input_username==user_name and input_password==password:
+#     print("login successfull")
+# elif input_username==user_name and input_password!=password:
+#     print("password is wrong")
+# elif input_username!=user_name and input_password==password:
+#     print("user name is wrong is wrong")       
+
+# else:
+#     print("inviled username and password")
+
+result="marks"
+name="Rahul Kumar" #math=70,hindi=80,science=67,sanskrit=68,social science=89
+Role_code="73069"
+Role_number="2200150"
+
+name="Kunal Kumar" #math=60,hindi=45,science=60,sanskrit=68,social science=46
+Role_code="73069"
+Role_number="2200151"
+
+name="Aditya Kumar" #math=77,hindi=88,science=67,sanskrit=65,social science=89
+Role_code="73069"
+Role_number="2200152" 
+
+name="Rani Kumari"  #math=70,hindi=74,science=57,sanskrit=68,social science=80
+Role_code="73069"
+Role_number="2200144"
+
+
+
