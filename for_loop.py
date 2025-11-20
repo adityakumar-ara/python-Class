9be57287d3e8207a5836878defeeb9db324d1ee3
Aditya Kumar <adityaara7667@gmail.com>
2025-11-21 00:43:48 +0530

    table

--- for_loop.py
+++
@@ -1,5 +0,0 @@
-for i in range(5,51,5):
-    print(i)
-
-for name in range(0,10):
-    print("aditya")   
---
+++ for_loop/for_loop.py
@@ -0,0 +1,6 @@
+# for i in range(5,51,5):
+#     print(i)
+
+# for name in range(0,10):
+#     print("aditya")   
+
