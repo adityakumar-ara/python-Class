93d979bd35b7a1f5c91a11b87e76c67288ece277
Aditya Kumar <adityaara7667@gmail.com>
2025-11-22 11:40:16 +0530

    odd even

---
+++ while_loop/using_whileLoop_even.py
@@ -0,0 +1,13 @@
+# i=2
+# while i<=30:
+#   print(i)
+#   i+=2
+# i=int(input("enter your number:"))
+# while i<=100:
+#     print(i)
+#     i+=2
+# i=int(input("enter your number:"))
+# n=int(input("enter your number:"))
+# while i<=n :
+#     print(i)
+#     i+=2
