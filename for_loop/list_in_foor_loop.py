666d59fb2a2cf920ffba5918647748ffe8963964
Aditya Kumar <adityaara7667@gmail.com>
2025-11-22 11:20:04 +0530

    list in loop

---
+++ for_loop/list_in_foor_loop.py
@@ -0,0 +1,18 @@
+# my_list=[1,2,33,444,5555,66666,"adita","kumar"]
+# for i in range(6):
+#   print(i)
+
+# my_list=[1,2,33,444,5555,66666,"adita","kumar"]
+# my_list.pop(1)
+# print(my_list)
+
+# my_list=[1,2,33,444,5555,66666,"adita","kumar"]
+# print("index of 444:",my_list.index(444))
+
+# my_list = [11, 22, 24, 33, 44, 100, 1000, 100, 77]
+# print("Before:", my_list)
+
+# my_list.sort()
+# print("After:", my_list)
+
+
