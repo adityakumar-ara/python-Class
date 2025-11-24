63d20ae0151dd3993f23b925574186e516d7b3ff
Aditya Kumar <adityaara7667@gmail.com>
2025-11-24 23:04:56 +0530

    tuple

---
+++ tuple_related/muteble.py
@@ -0,0 +1,13 @@
+# my_list=[1,2,33,44,55,66,"aditya","ujwal"]
+# my_list[6]='udit'  ##change
+# print(my_list)
+
+# my_name="aditya"
+# print(my_name[1])
+
+# my_name="aditya"
+# my_name[1]="A"
+# print(my_list)  ##it give will be error brcause this is immuteble ,there in immuteble not change  specifically
+# name="aditya"
+# name="udit kumar mourya"
+# print(name)
---
+++ tuple_related/tuple.py
@@ -0,0 +1,12 @@
+# my_tuple=(22,44,66,88,"aditya")
+# print(my_tuple)
+# print(my_tuple[4])
+# print(my_tuple[-1])
+
+
+# my_tuple=('aditya',22,54,67,9,'aditya')
+# print(my_tuple.count("aditya"))
+
+# my_tuple=(1,5,4,54,3,2,9)
+# print(my_tuple.index(4))
+# print(len(my_tuple))
