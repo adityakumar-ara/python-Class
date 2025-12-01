5f09cb6eb6cc3238d6a711e12300b886bbe343ce
Aditya Kumar <adityaara7667@gmail.com>
2025-12-01 23:44:27 +0530

    function

---
+++ tuple_related/function/sum.py
@@ -0,0 +1,34 @@
+# def sum(a,b):
+#     print(a+b)
+#     return a+b
+
+# sum(2,3)
+
+# def calc_sum(a,b,c):
+#    sum=a+b+c
+#    ave=sum/3
+#    print(sum)
+#    print(ave)
+#    return sum,ave
+
+# calc_sum(3,55,46)    
+
+
+# def sum(a,b,c):
+#     print(a+b+c)
+# sum (7,8,9)
+# sum(23,32,23)    
+    
+
+# def prod(a,b):
+#     print(a*b)
+
+# prod(4,6)
+# prod(7,6)   
+
+# cityname=["ara","buxar","bhojpur","jagdishpu"]
+# name=["adita","rahul","kunal","amrendra","vishal"]
+# def city_len(list):
+#     print(list)
+# city_len(cityname)
+# city_len(name)    
