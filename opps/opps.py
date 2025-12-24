be8a56ea63620e7921914f6338412aa3034dd1d5
Aditya Kumar <adityaara7667@gmail.com>
2025-12-24 23:45:59 +0530

    add more

--- opps/opps.py
+++ opps/opps.py
@@ -35,15 +35,52 @@
 # ______________________________________________Multiple Inheritence_____________________________________________________
 
 
-class ram_father :
-              property_of_father = "car, house , chocolate factory"
-class ram :
-        property_of_ram = "milk factory","rice factory"              
+# class ram_father :
+#               property_of_father = "car, house , chocolate factory"
+# class ram :
+#         property_of_ram = "milk factory","rice factory"              
 
-class sun_of_ram(ram_father ,ram) :
-            property_of_sun = "sugar factory"
+# class sun_of_ram(ram_father ,ram) :
+#             property_of_sun = "sugar factory"
 
-sun_obj = sun_of_ram()  
-print(sun_obj.property_of_father)          
-print(sun_obj.property_of_ram)          
-print(sun_obj.property_of_sun)          
+# sun_obj = sun_of_ram()  
+# print(sun_obj.property_of_father)          
+# print(sun_obj.property_of_ram)          
+# print(sun_obj.property_of_sun)          
+
+
+# ---------------------------------------Multilevel inharitance -------------------------------
+
+# class grandfather:
+#             grandfahter_assest = "car"
+# class father(grandfather):
+#         father_assest = "bike"
+# class son(father):
+#         son_assest = " luxury house"                    
+# son_obj = son()
+# print(son_obj.grandfahter_assest)        
+# print(son_obj.father_assest)        
+# print(son_obj.son_assest)        
+
+
+
+# class a4notebook:
+#      def about_a4notebook(self):
+#           print("page = 400")
+#           print("page_color = \"white\"")
+#           print("price = 100")
+# class a5notebook(a4notebook):
+#      def about_a5notebook(self):
+#          print("page = 200")
+#          print("page_color = \"white\"")
+#          print("price = 120")
+
+# class notebook(a5notebook):
+#      def about_notebook(self):
+#           print("page = 70")
+#           print("page_color = \"white\"")
+#           print("price = 20")
+# notebook_obj = notebook()
+# notebook_obj.about_a4notebook()
+# notebook_obj.about_a5notebook()
+# notebook_obj.about_notebook()
