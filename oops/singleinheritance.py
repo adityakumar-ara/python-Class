b3b5aa665c9a83e96bae29e9e6972159f7eb586e
Aditya Kumar <adityaara7667@gmail.com>
2026-02-06 07:35:00 +0530

    add more

---
+++ oops/singleinheritance.py
@@ -0,0 +1,9 @@
+# class father_pro():
+#     father_name = "suendra singh"
+#     father_pro = "car ,'two bigha of land', water park"
+# sun_obj = father_pro()
+# print(sun_obj.father_pro)
+# print(sun_obj.father_name)
+
+# ----------------------------------------------------------------------------------
+
