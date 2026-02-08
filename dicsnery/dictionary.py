07bd9f19e0912cac386ae3d28fc159ba3fed48a4
Aditya Kumar <adityaara7667@gmail.com>
2026-02-08 23:58:17 +0530

    update

--- dicsnery/dictionary.py
+++ dicsnery/dictionary.py
@@ -6,3 +6,11 @@ my_dict = {
 }
 print(my_dict.get("name"))
 print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.items())
+my_dict.update({"name":"aditya kushwaha"})
+print(my_dict)
+my_dict.update({"age":21})
+print(my_dict)
+my_dict.pop("name")
+print(my_dict.clear())
