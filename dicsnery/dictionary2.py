8f731735f11dcc067e1719cd886d098bc93727ca
Aditya Kumar <adityaara7667@gmail.com>
2026-02-09 12:18:19 +0530

    add more

---
+++ dicsnery/dictionary2.py
@@ -0,0 +1,38 @@
+my_dict = {
+    "name":"gulshan kumar",
+    "father name" : "surendra singh",
+    "village" : "khadaha tola ",
+    "post" : "hardiya",
+    "pin code": 802152,
+    "age" : 21,
+    "job" : "Private",
+    "salary" : 80000,
+}
+
+# print(my_dict)
+# print(my_dict.keys())
+# print(my_dict.get("village"))
+# print(my_dict.get("salary"))
+# print(my_dict.values())
+# print(my_dict.items())
+# my_dict.update({"name":"gulshan kushwaha"})
+# print(my_dict.get("name"))
+# my_dict.pop("salary")
+# print(my_dict.get("salary"))
+# print(my_dict.clear())
+
+# print(my_dict.get("name").upper())
+# print(my_dict.get("name").title())
+
+# for key,value in my_dict.items():
+#     if isinstance(value,str):
+#         print(f"{key.title()}:{value.title()}")
+#     else:
+#         # print(f"{key}:{value}")
+#         if isinstance(key,str):
+#             print(f"{key.title()}: {value}")
+#         else:
+#             print(key)    
+
+for value in my_dict.values():
+    print(str(value))
