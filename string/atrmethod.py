5beaad82129c7d8d88123c897f9f5a90ed40ee9f
Aditya Kumar <adityaara7667@gmail.com>
2026-02-10 13:55:54 +0530

    add more

--- string/atrmethod.py
+++ string/atrmethod.py
@@ -1,8 +1,17 @@
-name = "programming"
-print(len(name))
-print(name.upper())
-print(name.count("m"))
-print(name.title())
-print(name.capitalize())
-print(name.find("m"))
-print(name.index("m"))
+# name = "programming"
+# print(len(name))
+# print(name.upper())
+# print(name.count("m"))
+# print(name.title())
+# print(name.capitalize())
+# print(name.find("m"))
+# print(name.index("m"))
+# print(name.replace("p","P"))
+
+# text = "  Python is very easy  "
+# words = text.split()
+# print(words)
+
+
+# print(text.strip())
+# print(text.strip())
