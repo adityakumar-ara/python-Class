e3607c70f0a2866e307b06e7e3602cfc621d0248
Aditya Kumar <adityaara7667@gmail.com>
2026-02-21 11:46:27 +0530

    add more

---
+++ set,py/set.py
@@ -0,0 +1,16 @@
+my_set = {1,2,3,5,4,5,6,7,8,9}
+# print(type(number))
+# print(my_set)
+# print(number[6]) #it return erroe cause set not support indexing
+# my_set.add(10)
+# my_set.update({10,11,12,13})
+# my_set.remove(5)
+# print(my_set)
+
+# for i in my_set:
+#     print(i)
+
+# my_set.pop()
+# my_set.discard(4)
+# my_set.discard(44)
+print(my_set)
