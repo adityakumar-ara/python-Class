66cdc36f797f52961746b1c2df6e420152af1aa0
Aditya Kumar <adityaara7667@gmail.com>
2026-02-21 10:14:31 +0530

    add mmore

---
+++ tuple/tuple.py
@@ -0,0 +1,14 @@
+# number = (1,2,3,3,3,4,5,6,7)
+# print(number)
+# print(number[5])
+# print(number)
+# print(type(number))
+# print(number.count(3))
+# print(number.count(1))
+# print(number.index(1))
+# print(number.index(3))
+# print(len(number))
+
+# ----------------Negative indexing tuple-------------------------------------------
+    # -9 -8 -7 -6 -5 -4  -3 -2 -1
+n = (1,  2, 3, 4, 5, 6,  7, 8, 9)
