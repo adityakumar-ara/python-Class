42bbe10fe1cf44029815ca60ce6a7641049ad4cf
Aditya Kumar <adityaara7667@gmail.com>
2026-02-21 10:53:41 +0530

    add more

--- tuple/tuple.py
+++ tuple/tuple.py
@@ -13,9 +13,34 @@
   # -9  -8 -7 -6 -5 -4  -3 -2 -1
 # n = (1,  2, 3, 4, 5, 6,  7, 8, 9)
 
-n = (1,2,3,4,5,6,7,8,9)
-print(n)
-print(n[2])
-print(n[-2])
-slacing = n[0:10:2]
-print(slacing)
+# n = (1,2,3,4,5,6,7,8,9)
+# print(max(n))
+# print(min(n))
+# print(sum(n))
+# print(n)
+# print(n[2])
+# print(n[-2])
+# slacing = n[0:10:2]
+# print(slacing)
+
+# nu = (3,2,1,4,5,6,7,8)
+# print(sorted(nu))
+
+# -------------------------------unpacking------------------------------------------
+fruits = ("apple", "banana", "cherry")
+
+# Unpacking the tuple into three separate variables
+(green, yellow, red) = fruits
+
+print(green)  # Output: apple
+print(yellow) # Output: banana
+print(red)    # Output: cherry
+
+# ----------------------Advanced Unpacking: The Asterisk * (Star) Operator---------------------------
+grades = (100, 90, 85, 88, 72)
+
+(highest, *middle, lowest) = grades
+
+print(highest) # Output: 100
+print(middle)  # Output: [90, 85, 88]
+print(lowest)  # Output: 72
